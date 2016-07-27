34d185d7b532140c3b7da9fa0c2c7f161958fbc5
chpill <etienne@spillemaeker.com>
2016-07-27 15:49:58 +0200

    End of episode 1
    To make figwheel work, I used the same configuration has in the figwheel lein
    template.

--- project.clj
+++ project.clj
@@ -3,8 +3,22 @@
   :url "http://www.parens-of-the-dead.com"
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/gpl.html"}
-  :jvm-opts ["-XX:MaxPermSize=256m"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]]
-  :profiles {:dev {:plugins []
-                   :dependencies []
-                   :source-paths ["dev"]}})
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.93"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.6.0-beta1"]]
+  :main undead.system
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.1.3"]
+                             [lein-figwheel "0.5.4-1"]]
+                   :dependencies [[reloaded.repl "0.2.2"]]
+                   :source-paths ["dev"]
+                   :cljsbuild {:builds
+                               [{:id "dev"
+                                 :source-paths ["src"]
+                                 :figwheel true
+                                 :compiler {:main "undead.client"
+                                            :asset-path "js/compiled/out"
+                                            :output-to "resources/public/js/compiled/undead.js"
+                                            :output-dir "resources/public/js/compiled/out"
+                                            :source-map-timestamp true}}]}}})
---
+++ src/undead/system.clj
@@ -0,0 +1,28 @@
+(ns undead.system
+  (:require [com.stuartsierra.component :as component]
+            [org.httpkit.server :as httpkit]
+            [undead.web :refer [app]]))
+
+(defn- start-server [handler port]
+  (let [server (httpkit/run-server handler {:port port})]
+    (println (str "Started undead server on http://localhost:" port))
+    server))
+
+(defn- stop-server [server]
+  ;; run-server returns a fn that stops the server it just started
+  (when server (server)))
+
+(defrecord ParensOfTheDead []
+    component/Lifecycle
+  ;; TODO check if there is already a running server?
+  (start [this]
+    ;; var quote app to avoid stale definitions
+    (assoc this :server (start-server #'app 8080)))
+  (stop [this]
+    (stop-server (:server this))
+    (dissoc this :server)))
+
+(defn create-system [] (ParensOfTheDead.))
+
+(defn -main [& args]
+  (.start (create-system)))
