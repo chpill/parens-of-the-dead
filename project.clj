b4fd22156071ef07f5193f45102e45d44f403377
chpill <etienne@spillemaeker.com>
2016-07-27 16:57:29 +0200

    End of episode 2

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [org.clojure/clojurescript "1.9.93"]
                  [com.stuartsierra/component "0.3.1"]
                  [http-kit "2.2.0"]
-                 [compojure "1.6.0-beta1"]]
+                 [compojure "1.6.0-beta1"]
+                 [quiescent "0.3.2"]]
   :main undead.system
   :profiles {:dev {:plugins [[lein-cljsbuild "1.1.3"]
                              [lein-figwheel "0.5.4-1"]]
--- src/undead/client.cljs
+++ src/undead/client.cljs
@@ -1,3 +1,43 @@
-(ns undead.client)
+(ns undead.client
+  (:require [quiescent.core :as qt]
+            [quiescent.dom :as dom]))
 
-(js/alert "brains")
+(def game {:board [{:face :h1} {:face :h1} {:face :h2} {:face :h2}
+                   {:face :h3} {:face :h3} {:face :h4} {:face :h4}
+                   {:face :h5} {:face :h5} {:face :fg} {:face :fg}
+                   {:face :zo} {:face :zo} {:face :zo} {:face :gy}]
+           :sand (concat (repeat 70 :remaining)
+                         (repeat 20 :gone))
+           :foggy? false})
+
+(qt/defcomponent Cell [cell]
+  (dom/div {:className "cell"}
+           (dom/div {:className (str "tile"
+                                     (when (:revealed? cell) " revealed")
+                                     (when (:matched? cell) " matched"))}
+                    (dom/div {:className "front"})
+                    (dom/div {:className (str "back " (name (:face cell)))}))))
+
+(qt/defcomponent Line [line]
+  (apply dom/div {:className "line"}
+         (map Cell line)))
+
+(qt/defcomponent Board [cells]
+  (apply dom/div {:className "board clearfix"}
+         (map Line (partition 4 cells))))
+
+(qt/defcomponent Timer [{:keys [index sand]}]
+  (apply dom/div {:className (str "timer timer-" index)}
+         (map #(dom/div {:className (str "sand " (name %))}) sand)))
+
+(qt/defcomponent Timers [sand]
+  (apply dom/div {}
+         (map-indexed #(Timer {:index %1 :sand %2}) (partition 30 sand))))
+
+(qt/defcomponent Game [game]
+  (dom/div {:className (when (:foggy? game) "foggy")}
+           (Board (:board game))
+           (Timers (:sand game))))
+
+(qt/render (Game game)
+           (.getElementById js/document "main"))
