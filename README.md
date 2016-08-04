1269ea628e847d29c393cf0764616724b41a9d04
chpill <etienne@spillemaeker.com>
2016-08-05 00:05:11 +0200

    First attempt at loading component configuration.
    This is very ad hoc (but very brief). Duct seems to have some interesting
    plumbing in terms of configuration.

--- README.md
+++ README.md
@@ -15,10 +15,11 @@ In this repo, each commit is following along with the awesome [parens-of-the-dea
 
 * [X] catch uncaught exceptions globally for the server (got bitten by a really bad case of https://stuartsierra.com/2015/05/27/clojure-uncaught-exceptions)
 * [X] use transit to communicate the game to the client
+* [\] load magic numbers from config, make it play nice with the web server component
 * [ ] add proper logging? (related to the previous item)
 * [ ] use devcards to illustrate the UI
-* [ ] load magic numbers from config, make it play nice with the web server component
 * [ ] wrap the web-socket in a component on the client?
-* [ ] try to use aleph
+* [ ] launch figwheel in the same jvm as the repl
 * [ ] build a health-check endpoint
 * [ ] build a reporting endpoint (how many active ws? other stats?)
+* [ ] try to use aleph?
--- src/undead/system.clj
+++ src/undead/system.clj
@@ -1,7 +1,9 @@
 (ns undead.system
   (:require [com.stuartsierra.component :as component]
             [org.httpkit.server :as httpkit]
-            [undead.web :refer [app]]))
+            [undead.web :refer [app]]
+            [clojure.java.io :as io]
+            [clojure.tools.reader.edn :as edn]))
 
 (def global-exception-catcher
   (Thread/setDefaultUncaughtExceptionHandler
@@ -18,17 +20,23 @@
   ;; run-server returns a fn that stops the server it just started
   (when server (server)))
 
-(defrecord ParensOfTheDead []
-    component/Lifecycle
+(defrecord HttpServer [config]
+  component/Lifecycle
   ;; TODO check if there is already a running server?
   (start [this]
     ;; var quote app to avoid stale definitions
-    (assoc this :server (start-server #'app 9009)))
+    (assoc this :server (start-server #'app (:port config))))
   (stop [this]
     (stop-server (:server this))
     (dissoc this :server)))
 
-(defn create-system [] (->ParensOfTheDead))
+(defn read-config [source]
+  (-> source io/resource slurp edn/read-string))
 
-(defn -main [& args]
-  (.start (create-system)))
+(defn create-system []
+   (let [config (read-config "dev.edn")]
+     (println "reading config" config)
+     (->HttpServer (:http-server config))))
+
+(defn -main [& args] (.start (create-system)))
+
